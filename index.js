cf5c74d79d1e8685ccba5837859b01c2da22733d
James Heiniger <james.heiniger911@gmail.com>
2025-10-30 14:03:20 +1030

    form now only highlights one option

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ import { catsData } from "/data.js";
 
 const emotionRadios = document.getElementById("emotion-radios");
 
+emotionRadios.addEventListener("change", highlightCheckedOption);
+
+function highlightCheckedOption(e) {
+  const radios = document.getElementsByClassName('radio')
+  for (let radio of radios) {
+    radio.classList.remove('highlight')
+  }
+  document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+
+
 function getEmotionsArray(arr) {
   const emotionsArray = [];
   arr.forEach(cat => {
     cat.emotionTags.forEach(tag => {
-      if (emotionsArray.includes(tag)) {
-        
-      } else {
+      if (!emotionsArray.includes(tag)) {
         emotionsArray.push(tag);
       }
-      
     });
   });
   return emotionsArray;
